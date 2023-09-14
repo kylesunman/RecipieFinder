0f32092809a4dfce41e2f9a5dcb3bd0f4e9dad11
Kyle Sunman <kylesunman123@gmail.com>
2023-09-14 19:46:17 -0400

    added javascript to add function to the add ingredient button

--- javascript/script.js
+++ javascript/script.js
@@ -2,28 +2,6 @@
 const ingredientInput = document.getElementById("ingredient");
 const ingredientList = document.getElementById("ingredient-list");
 const recipeList = document.getElementById("recipe-list");
-
-// Function to fetch recipes from the API based on ingredients
-function fetchRecipes(ingredients) {
-    // Replace 'YOUR_API_KEY' with your actual API key
-    const apiKey = 'YOUR_API_KEY';
-    const baseUrl = 'https://api.example.com/recipes'; // Replace with the API endpoint
-
-    // Build the API URL with the ingredients
-    const apiUrl = `${baseUrl}?ingredients=${ingredients.join(',')}&apiKey=${apiKey}`;
-
-    // Make a GET request to the API
-    fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => {
-            // Handle the data and display recipes
-            displayRecipes(data.recipes);
-        })
-        .catch(error => {
-            console.error('Error fetching recipes:', error);
-        });
-}
-
 // Function to display recipes on the web page
 function displayRecipes(recipes) {
     // Clear the previous recipes
@@ -55,3 +33,25 @@ document.getElementById("add-ingredient").addEventListener("click", function () 
         fetchRecipes(ingredientsArray);
     }
 });
+function fetchRecipes() {
+  const apiKey = "5ae04e38092e84803117d87ec2025d5e"; // Replace with your actual API key
+  const ingredientInput = document.getElementById("ingredient");
+  const ingredientText = encodeURIComponent(ingredientInput.value.trim()); // Get the ingredient text from the input field
+  const recipeList = document.getElementById("recipe-list"); // Assuming your HTML structure matches the provided HTML
+
+  const apiUrl = `https://api.example.com/recipes?ingredients=${ingredientText}&apiKey=${apiKey}`;
+
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      recipeList.innerHTML = ""; // Clear previous recipe list
+      data.recipes.forEach((recipe) => {
+        const listItem = document.createElement("li");
+        listItem.textContent = recipe.name;
+        recipeList.appendChild(listItem);
+      });
+    })
+    .catch((error) => {
+      console.error("Error fetching recipes:", error);
+    });
+}
