60af990f26bc67e54f7e357555c88c810715780e
LanceLancealot <maxfield.kraus@gmail.com>
2023-09-14 19:19:38 -0400

    Added api key and api fetch

--- javascript/script.js
+++ javascript/script.js
@@ -1,22 +1,22 @@
 function fetchRecipes() {
-    const apiKey = "YOUR_API_KEY"; // Replace with your actual API key
-    const ingredientInput = document.getElementById("ingredient");
-    const ingredientText = encodeURIComponent(ingredientInput.value.trim()); // Get the ingredient text from the input field
-    const recipeList = document.getElementById("recipe-list"); // Assuming your HTML structure matches the provided HTML
+  const apiKey = "5ae04e38092e84803117d87ec2025d5e"; // Replace with your actual API key
+  const ingredientInput = document.getElementById("ingredient");
+  const ingredientText = encodeURIComponent(ingredientInput.value.trim()); // Get the ingredient text from the input field
+  const recipeList = document.getElementById("recipe-list"); // Assuming your HTML structure matches the provided HTML
 
-    const apiUrl = `https://api.example.com/recipes?ingredients=${ingredientText}&apiKey=${apiKey}`;
+  const apiUrl = `https://api.example.com/recipes?ingredients=${ingredientText}&apiKey=${apiKey}`;
 
-    fetch(apiUrl)
-        .then((response) => response.json())
-        .then((data) => {
-            recipeList.innerHTML = ""; // Clear previous recipe list
-            data.recipes.forEach((recipe) => {
-                const listItem = document.createElement("li");
-                listItem.textContent = recipe.name;
-                recipeList.appendChild(listItem);
-            });
-        })
-        .catch((error) => {
-            console.error("Error fetching recipes:", error);
-        });
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      recipeList.innerHTML = ""; // Clear previous recipe list
+      data.recipes.forEach((recipe) => {
+        const listItem = document.createElement("li");
+        listItem.textContent = recipe.name;
+        recipeList.appendChild(listItem);
+      });
+    })
+    .catch((error) => {
+      console.error("Error fetching recipes:", error);
+    });
 }
