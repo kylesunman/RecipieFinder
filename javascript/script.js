531ba55ba756d3ff20ed0061a21e2baabbeab8d5
LanceLancealot <maxfield.kraus@gmail.com>
2023-09-14 19:58:03 -0400

    typo change

--- javascript/script.js
+++ javascript/script.js
@@ -56,25 +56,3 @@ function fetchRecipes() {
     });
 }
 
-function fetchRecipes() {
-  const apiKey = "5ae04e38092e84803117d87ec2025d5e"; 
-  const ingredientInput = document.getElementById("ingredient");
-  const ingredientText = encodeURIComponent(ingredientInput.value.trim()); 
-  const recipeList = document.getElementById("recipe-list"); 
-
-  const apiUrl = `https://api.example.com/recipes?ingredients=${ingredientText}&apiKey=${apiKey}`;
-
-  fetch(apiUrl)
-    .then((response) => response.json())
-    .then((data) => {
-      recipeList.innerHTML = ""; // Clear previous recipe list
-      data.recipes.forEach((recipe) => {
-        const listItem = document.createElement("li");
-        listItem.textContent = recipe.name;
-        recipeList.appendChild(listItem);
-      });
-    })
-    .catch((error) => {
-      console.error("Error fetching recipes:", error);
-    });
-}
